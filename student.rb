7d6d3ecfd09022a684904f6072a7842a50ed22a7
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-07 09:59:19 -0500

    Create 'student.rb' file agree with requirements

---
+++ student.rb
@@ -0,0 +1,11 @@
+class Student < Person
+
+  def initialize(name: "Unknown", age, classroom, parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
