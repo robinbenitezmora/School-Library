783a8b6a7481aabb3ccd2398907e8bdac233f13f
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-08 10:13:37 -0500

    Create associations among Book, Rental and Person classes

--- src/book.rb
+++ src/book.rb
@@ -1,9 +1,17 @@
+require_relative './person'
+require_relative './rental'
+
 class Book 
   attr_accessor :date
   attr_accessor :title
+  attr_accessor :rental
 
-  def initialize(title, date)
+  def initialize(title, author)
     @title = title
-    @date = date
+    @author = author
+    @rental = nil
   end
+
+  def add_rental(person, date)
+    Rental.new(date, person, self)
 end
