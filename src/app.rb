ebaabfd66f5d0816383b451642c804dd302be6a4
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-08 16:43:46 -0500

    Modify 'main.rb' and 'app.rb'

--- src/app.rb
+++ src/app.rb
@@ -12,41 +12,6 @@ class App
     @rentals = []
   end
 
-  def run
-    puts 'Welcome to School Library App!'
-    loop do
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      option = gets.chomp
-
-      case option
-      when '1'
-        list_all_books
-      when '2'
-        list_all_people
-      when '3'
-        create_person
-      when '4'
-        create_book
-      when '5'
-        create_rental
-      when '6'
-        list_rentals_by_person_id
-      when '7'
-        puts 'Thank you for using this app!'
-        break
-      else
-        puts 'That is not a valid option'
-      end
-    end
-  end
-
   private
 
   def list_all_books
