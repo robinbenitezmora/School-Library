d7ae85127d87185148a52af8c4f79e5f05254f3f
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-08 16:23:58 -0500

    Create 'main.rb' file with general menu for the application's users

---
+++ src/main.rb
@@ -0,0 +1,44 @@
+require_relative './app'
+
+data = { books: [], people: [], rentals: [] }
+
+puts 'Welcome to School Library App!'
+
+def menu
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+  gets.chomp
+end
+
+def main_menu(db_data)
+  app = App.new
+  loop do
+    case menu
+    when '1'
+      app.list_all_books(db_data[:books])
+    when '2'
+      app.list_all_people(db_data[:people])
+    when '3'
+      app.create_person(db_data[:people])
+    when '4'
+      app.create_book(db_data[:books])
+    when '5'
+      app.create_rental(db_data[:rentals], db_data[:books], db_data[:people])
+    when '6'
+      app.list_rentals_by_person_id(db_data[:rentals], db_data[:books], db_data[:people])
+    when '7'
+      puts 'Thank you for using this app!'
+      break
+    else
+      puts 'That is not a valid option'
+    end
+  end
+end
+
+main_menu(data)
