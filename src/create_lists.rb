17d65e96220f0e30bd5e596b077c3912b520a898
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-14 14:39:30 -0500

    Correct code lines in 'list_rentals' method

--- src/create_lists.rb
+++ src/create_lists.rb
@@ -18,20 +18,18 @@ class Lists
   end
 
   def list_rentals(db_people)
-    def list_rentals(db_people)
-      if db_people.empty?
-        puts 'There are no people to list rentals'
-        return
-      end
-  
-      puts 'Please enter the ID of the person whose rentals you want to see'
-      id = gets.chomp.to_i
-  
-      user = db_people.find { |person| person.id == id }
-      puts 'Rentals:'
-      user.rentals.each do |rental|
-        puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
-      end
+    if db_people.empty?
+      puts 'There are no people to list rentals'
+      return
+    end
+
+    puts 'Please enter the ID of the person whose rentals you want to see'
+    id = gets.chomp.to_i
+
+    user = db_people.find { |person| person.id == id }
+    puts 'Rentals:'
+    user.rentals.each do |rental|
+      puts "Person: #{rental.name}, Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
     end
   end
 end
